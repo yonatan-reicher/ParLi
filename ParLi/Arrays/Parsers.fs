3a6801d787bf8bc06ac94454d2fec295d4ba600e
Yonatan Reicher <yony252525@gmail.com>
2020-11-08 14:45:22 +0200

    Added ParLi.Arrays.Parsers.get and ParLi.Arrays.Parsers.pop

--- ParLi/Arrays/Parsers.fs
+++ ParLi/Arrays/Parsers.fs
@@ -1,3 +1,9 @@
 ﻿[<AutoOpen>]
 module ParLi.Arrays.Parsers
 
+open ParLi.Parsers
+open ParLi.Linear
+
+let inline get<'t, 'S> : Parser<'t, 't array Input, 'S> = Parsers.get ()
+let inline pop<'t, 'S> : Parser<'t, 't array Input, 'S> = Parsers.pop ()
+
